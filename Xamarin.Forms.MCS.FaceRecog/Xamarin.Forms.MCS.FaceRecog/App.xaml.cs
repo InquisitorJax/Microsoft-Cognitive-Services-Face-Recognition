1bce89f3bff0d992dc2446eb3bbaf8a94845f947
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-14 10:44:39 +0200

    initial project commit

---
+++ Xamarin.Forms.MCS.FaceRecog/Xamarin.Forms.MCS.FaceRecog/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Xamarin.Forms.MCS.FaceRecog
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FaceRecog.MainPage());
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+    }
+}
